85446831e5f606901295241037e650cd1de78420
Carl Barcenas <carlanthonybarcenas@gmail.com>
2024-11-08 14:29:59 -0600

    Added navbar list item selection

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Temporary ReadMe File to track what's left to do:
+* Swap list items like a wheel.
+* Add list item functionality.
---
+++ script.js
@@ -0,0 +1,11 @@
+// Create selectors for navbar items
+const navbar_items = document.querySelectorAll("li");
+var active_nav = document.querySelector("#active");
+
+navbar_items.forEach((listitem) => {
+    listitem.addEventListener("click", () => {
+        listitem.setAttribute("id", "active");
+        active_nav.removeAttribute("id");
+        active_nav = document.querySelector("#active");
+    })
+})
