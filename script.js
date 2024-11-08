4c2f61cd62ec68612af3fd075b1c9c02dbf25204
Carl Barcenas <carlanthonybarcenas@gmail.com>
2024-11-08 14:42:16 -0600

    Fixed bug where multiple navbar clicks on the same button breaks navbar active

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ var active_nav = document.querySelector("#active");
 
 navbar_items.forEach((listitem) => {
     listitem.addEventListener("click", () => {
-        listitem.setAttribute("id", "active");
-        active_nav.removeAttribute("id");
-        active_nav = document.querySelector("#active");
+        if(listitem.getAttribute("id") != "active") {
+            listitem.setAttribute("id", "active");
+            active_nav.removeAttribute("id");
+            active_nav = document.querySelector("#active");
+        }
+        else {
+            console.log("pingpong");
+        }
+        
     })
 })
