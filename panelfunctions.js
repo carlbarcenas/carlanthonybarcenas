66d54024bc451e3fe67c4621933ecf75430a94a8
Carl Barcenas <carlanthonybarcenas@gmail.com>
2024-11-15 16:02:32 -0600

    Added pop-in animation and empty navbar status.

--- panelfunctions.js
+++ panelfunctions.js
@@ -1,5 +1,11 @@
 function clearPanel(panelID) {
-    document.getElementById(panelID).innerHTML = "";
+    // Animation for panel clearing:
+    var panel = document.getElementById(panelID);
+    panel.classList.add("pop-out");
+    panel.addEventListener("animationend", function() {
+        panel.innerHTML = "";
+        panel.classList.remove("pop-out");
+    })
 }
 
 function aboutMe() {
@@ -7,8 +13,7 @@ function aboutMe() {
     const leftPanel = document.querySelector(".leftpanel");
     const image = document.createElement("img");
     image.src = 'images/grad_photo.jpg';
-    image.setAttribute("class", "selfImage");
-    image.setAttribute("class", "pop-in"); // slide-in or pop-in?
+    image.classList.add("selfImage", "pop-in");
     image.style.width = "30vw";
     image.style.height = "auto";
     image.style.borderRadius = "40px";
@@ -18,6 +23,7 @@ function aboutMe() {
     // Create right panel of self description
     const rightPanel = document.querySelector(".rightpanel");   // Get Right Panel
     const aboutText = document.createElement("h1");             // Create and style about me text
+    aboutText.classList.add("aboutMeText", "pop-in");
     aboutText.setAttribute("class", "aboutMeText");
     aboutText.setAttribute("class", "pop-in");
     aboutText.textContent = "Marquette University Alumni. Software Engineer. IT Professional."
--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // Create selectors for navbar items
 const navbar_items = document.querySelectorAll("li");
 var active_nav = document.querySelector("#active");
+console.log(active_nav);
 
 navbar_items.forEach((listitem) => {
     listitem.addEventListener("click", () => {
-        if(listitem.getAttribute("id") != "active") {
+        if(listitem.getAttribute("id") != "active" || active_nav == null) {
             listitem.setAttribute("id", "active");              // Set clicked item as "active" ID
-            active_nav.removeAttribute("id");                   // Remove "active" ID from old selection
+            if(active_nav != null) {
+                active_nav.removeAttribute("id");
+            }                   // Remove "active" ID from old selection
             active_nav = document.querySelector("#active");     // Update active_nav variable
             
             // Delete current panels
@@ -17,13 +20,10 @@ navbar_items.forEach((listitem) => {
             switch (active_nav.className) {
                 case 'aboutme':
                     aboutMe();
+                    console.log("aboutme printing");
                     break;
             }
 
-
-            // DELETE ME, used for debugging
-            console.log(active_nav);
-            console.log(active_nav.className);
         }
     })
 })
