3eba1c58839d8fb87537a5f3e76c465378c1aeae
Carl Barcenas <carlanthonybarcenas@gmail.com>
2024-11-15 16:02:55 -0600

    Added pop-in animation and empty nav bar status 2

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ navbar_items.forEach((listitem) => {
     listitem.addEventListener("click", () => {
         if(listitem.getAttribute("id") != "active" || active_nav == null) {
             listitem.setAttribute("id", "active");              // Set clicked item as "active" ID
-            if(active_nav != null) {
+            if(active_nav != null) {                            // Remove "active" ID from old selection
                 active_nav.removeAttribute("id");
-            }                   // Remove "active" ID from old selection
+            }
             active_nav = document.querySelector("#active");     // Update active_nav variable
             
             // Delete current panels
